a9d38f96570d29934b80d6862004123a8ca606b8
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-23 18:36:19 -0300

    Exercicio 4 - Resolvido

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicio_1.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicio_1.js
@@ -1,6 +1,5 @@
 let a = 10;
 let b = 15;
-let c = 20;
 
 console.log('Soma: ' + (a + b));
 console.log('Subtração: ' + (a - b));
@@ -8,11 +7,18 @@ console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
 
+let a = 10;
+let b = 15;
+
 if (a > b) {
     console.log(a);
 }else{
     console.log(b);
 }
+
+let a = 10;
+let b = 15;
+let c = 20;
 
 if(a > b && a > c){
     console.log(a)
@@ -21,3 +27,14 @@ if(a > b && a > c){
 }else{
     console.log(c)
 }
+
+let a = 10;
+
+if(a > 0) {
+    console.log('Positive')
+}else if(a < 0){
+    console.log('Negative')
+}else{
+    console.log('Zero')
+}
+

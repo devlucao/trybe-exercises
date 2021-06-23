a56fecaa76cdfdb70c12e55c6fca09de15883783
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-23 18:18:44 -0300

    Exercicio 1 - Resolvido

---
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicio_1.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 15;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
+
+
+

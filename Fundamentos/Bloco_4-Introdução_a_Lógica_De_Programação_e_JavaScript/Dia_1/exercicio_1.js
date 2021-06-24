d7bb1a39f48a959a0ddeb5e4270aedb139dd2aa8
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-23 22:47:37 -0300

    Exercicio 7 - Resolvido

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicio_1.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicio_1.js
@@ -91,3 +91,21 @@ switch (pecaDeXadrez.toLowerCase()) {
     console.log("Erro, peça não existe");
 }
 
+let nota = 100;
+
+if (nota >= 90 && nota <= 100) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >= 50) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+        console.log("F");
+}else{
+    console.log('Erro, nota inválida.')
+}
+

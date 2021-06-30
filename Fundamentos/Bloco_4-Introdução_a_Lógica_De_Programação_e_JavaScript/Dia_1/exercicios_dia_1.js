a9a9667e97587b0bff2f104bef34c281870b11f7
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 10:39:16 -0300

    Resolve exercicio 8

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
@@ -105,3 +105,16 @@ if(nota >= 90){
 } else {
     console.log("F")
 }
+
+//Exercício 8
+let num1 = 11;
+let num2 = 21;
+let num3 = 31;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// Exercicio 9

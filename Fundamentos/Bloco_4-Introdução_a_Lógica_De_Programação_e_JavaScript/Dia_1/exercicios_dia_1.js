88ba9ccf4123fc6e55d764f2b09d234d6a4976c3
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 10:41:10 -0300

    Resolve exercicio 9

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
@@ -118,3 +118,14 @@ if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
 }
 
 // Exercicio 9
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// Exercicio 10

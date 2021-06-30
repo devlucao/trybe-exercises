02ae59084f7bc7746cf839fd0d952449a0623d57
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 10:36:05 -0300

    Resolve exercicio 7

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
@@ -90,3 +90,18 @@ switch(pecaXadrez.toLowerCase()) {
 }
 
 // Exercicio 7
+
+let nota = 35;
+if(nota >= 90){
+    console.log("A");
+} else if (nota >= 80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >= 50) {
+    console.log("E");
+} else {
+    console.log("F")
+}

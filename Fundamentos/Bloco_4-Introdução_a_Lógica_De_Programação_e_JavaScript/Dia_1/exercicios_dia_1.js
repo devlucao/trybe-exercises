b45321955007842f681ad4cdf468de14043fee4c
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 00:21:31 -0300

    Resolve exercicios das partes II e III

---
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_1/exercicios_dia_1.js
@@ -0,0 +1,19 @@
+// Exercício Parte II - Dia 1
+
+// const myName = "Lucas";
+// const birthCity = "Rio de Janeiro";
+// let birthYear = 1991;
+// console.log(myName, birthCity, birthYear);
+// birthYear = 2030;
+// console.log(birthYear);
+
+// Exercício Parte III - Dia 1
+
+let base = 5;
+let altura = 8;
+
+let area = base * altura;
+let perimetro = (base *2) + (altura *2);
+console.log(area);
+
+//Exercício Parte IV - Dia 1

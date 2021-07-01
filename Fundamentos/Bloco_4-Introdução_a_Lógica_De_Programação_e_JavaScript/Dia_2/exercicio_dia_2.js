8ce03cecdf703ab3d7c5991e660ef4686102e98d
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 23:16:19 -0300

    Resolve exercicios 4 e 5

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/exercicio_dia_2.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/exercicio_dia_2.js
@@ -22,3 +22,24 @@ let media = sum / numbers.length;
 console.log(media);
 
 // Exercício 4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+sum = 0;
+for(i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+}
+let media = sum / numbers.length;
+if (media > 20) {
+    console.log('Valor maior que 20.');
+} else {
+    console.log("Valor menor ou igual a 20.");
+}
+
+// Exercício 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = numbers[0];
+for(i = 0; i < numbers.length; i += 1){
+    if(numbers[i] > maiorValor) {
+        maiorValor = numbers[i];
+    }
+}
+console.log(maiorValor);

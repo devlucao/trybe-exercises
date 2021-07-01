7c7dd5f8ae93d65fe339981dee5ec9d8f7ec0984
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-01 19:50:08 -0300

    Resolve Exercicio 1

---
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_3/Exercicios_dia3.js
@@ -0,0 +1,16 @@
+//Exercício 1
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for(let i = 0; i < n; i += 1) {
+    inputLine = inputLine + symbol;
+} 
+
+for(let i = 0; i < n; i += 1){
+    console.log(inputLine);
+}
+
+//Exercício 2
+
+   

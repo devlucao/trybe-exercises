5883aeca15becbb792183328a2a99482d1aa9174
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-01 15:59:32 -0300

    Resolve o exercicio Bonus Dia 2

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/exercicio_dia_2.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/exercicio_dia_2.js
@@ -84,3 +84,14 @@ let array =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 for (i = 0; i < array.length; i +=1) {
     console.log(array[i] / 2);
 }
+
+// Exercício Bônus
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newArray = [];
+for(let i = 0; i < array.length; i +=1) {
+    if(i + 1 < array.length) {
+        newArray.push(array[i] * array[i + 1]);
+        } else {
+            newArray.push(array.[i] * 2);
+        }
+}

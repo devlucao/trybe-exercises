fbe1c0e8ac802912c8c6ed0f8b44fe0b08d311ad
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 22:45:13 -0300

    Resolve Exercicio 1

---
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/fixacao_for_dia2.js
@@ -0,0 +1,11 @@
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+for(index = 0; index < listaDeNomes.length; index += 1) {
+    let mensagem = 'Bem vindo(a), ' + listaDeNomes.sort()[index] + '!'
+console.log(mensagem);
+}
+
+//Exercício 1
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(index = 0; index < groceryList.length; index +=1) {
+    console.log(groceryList[index]);
+}

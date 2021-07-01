2b462104a277f634f9945bfdc7495f7e93ba1bc9
Lucas Freitas <lucasr.freitas@icloud.com>
2021-06-30 23:06:34 -0300

    Resolve exercicios 1,2 e 3

---
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_2/exercicio_dia_2.js
@@ -0,0 +1,24 @@
+//Exercício 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+}
+
+// Exercicio 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for(i=0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+}
+console.log(sum);
+
+//Exercício 3
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+sum = 0;
+for(i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+}
+let media = sum / numbers.length;
+console.log(media);
+
+// Exercício 4

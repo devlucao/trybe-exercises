02dfcdc6582ca421664edb01e5d32d9f0bb1f930
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-08 11:34:38 -0300

    Resolve exercicio 3

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_3/Exercicios_dia3.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_3/Exercicios_dia3.js
@@ -1,33 +1,34 @@
-//Exercício 1
+// //Exercício 1
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for(let i = 0; i < n; i += 1) {
+//     inputLine = inputLine + symbol;
+// } 
+
+// for(let i = 0; i < n; i += 1){
+//     console.log(inputLine);
+// }
+
+// //Exercício 2
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for(let i = 0; i <= n; i += 1) {
+//     console.log(inputLine);
+//     inputLine = inputLine + symbol;
+// }
+
+//Exercício 3
+
 let n = 5;
-let symbol = '*';
-let inputLine = '';
-
-for(let i = 0; i < n; i += 1) {
-    inputLine = inputLine + symbol;
-} 
-
+let space = " ";
+let ast = "";
 for(let i = 0; i < n; i += 1){
-    console.log(inputLine);
-}
+    ast = ast + "*";
+    console.log(space.repeat(n - i) + ast);
+ }
 
-//Exercício 2
-let n = 5;
-let symbol = '*';
-let inputLine = '';
-
-for(let i = 0; i <= n; i += 1) {
-    console.log(inputLine);
-    inputLine = inputLine + symbol;
-}
-
-//Exercício 2
-let n = 5;
-let symbol = '*';
-let inputLine = '';
-
-for(let i = 0; i <= n; i += 1) {
-    console.log(inputLine);
-    inputLine = inputLine + symbol;
-}
-
+// Exercício 4

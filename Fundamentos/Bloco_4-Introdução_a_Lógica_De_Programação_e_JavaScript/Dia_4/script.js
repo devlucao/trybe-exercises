b2f9f300af087c40a3e3f1512e23e458810c244c
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-08 22:28:41 -0300

    Resolve exercicios For/In

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/script.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/script.js
@@ -1,3 +1,5 @@
+//Exercícios Objetos
+
 let player = { 
     name: "Marta",
     lastName: "Silva",
@@ -15,3 +17,30 @@ console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a
 
 console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro" + " " + "e " + player.medals.silver + " medalhas de prata.");
 
+// Exercícios ForIn
+
+let pizzas = {
+    sabor:"Palmito",
+    preco: 39.90,
+    bordaCatupiry: true
+};
+
+for(let key in pizzas) {
+    console.log(pizzas[key]);
+}
+
+let pizzasDoces = ['chocolate', 'banana', 'morango'];
+
+for (let key in pizzasDoces) {
+    console.log(key, pizzasDoces[key]);
+}
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+  for(let index in names) {
+      console.log("Olá " + names[index]);
+  }

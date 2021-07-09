49599d8328793570ea6819190b1e49449f240d95
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-08 23:42:44 -0300

    Resolve exercicio 1

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/exercicios_dia4.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/exercicios_dia4.js
@@ -19,33 +19,48 @@
 
 //   console.table(info);
 
-  //Exercícios 3 e 4
+//   //Exercícios 3 e 4
 
-  let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: "sim"
-  };
+//   let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: "sim"
+//   };
 
-//   for(key in info) {
-//   console.log(key);
-//   console.log(key, info[key]);
-//   }
+// //   for(key in info) {
+// //   console.log(key);
+// //   console.log(key, info[key]);
+// //   }
 
-//Exercício 5
+// //Exercício 5
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: "sim"
-  };
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: "sim"
+//   };
 
-  for(let key in info) {
-      if(key === 'recorrente' && info[key] === "sim" && info2[key] === "sim") {
-          console.log("Ambos recorrentes");
-      } else {
-          console.log(info[key] + " e " + info2[key]);
-      }
-    }
+//   for(let key in info) {
+//       if(key === 'recorrente' && info[key] === "sim" && info2[key] === "sim") {
+//           console.log("Ambos recorrentes");
+//       } else {
+//           console.log(info[key] + " e " + info2[key]);
+//       }
+//     }
+
+//--------Parte 2 - Funções
+//Exercício 1
+function verificaPalindromo(string) {
+  let palindromo = string.split("").reverse().join("");
+  if (palindromo === string){
+      return true;
+  } else {
+      return false;
+  }
+}
+
+console.log(verificaPalindromo("arara"));
+
+// Exercício 2

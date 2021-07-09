d8db72488b80de0fe0b75c188693a220a0aa9513
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-08 23:21:55 -0300

    Resolve exercicios de 3 a 5

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/exercicios_dia4.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/exercicios_dia4.js
@@ -1,25 +1,51 @@
-  //Exercício 1
+//   //Exercício 1
+
+//   let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   console.log("Seja bem vinda " + info.personagem);
+
+//  //Exercício 2
+
+//  let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: "sim"
+//   };
+
+//   console.table(info);
+
+  //Exercícios 3 e 4
 
   let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-
-  console.log("Seja bem vinda " + info.personagem);
-
- //Exercício 2
-
- let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: "sim"
   };
 
-  console.table(info);
+//   for(key in info) {
+//   console.log(key);
+//   console.log(key, info[key]);
+//   }
 
-  //Exercício 3
+//Exercício 5
 
-  
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: "sim"
+  };
 
+  for(let key in info) {
+      if(key === 'recorrente' && info[key] === "sim" && info2[key] === "sim") {
+          console.log("Ambos recorrentes");
+      } else {
+          console.log(info[key] + " e " + info2[key]);
+      }
+    }

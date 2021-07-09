3d56c22f18865011c615b61e4a1d1f8980d85018
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-08 22:59:30 -0300

    Adiciona exercicio funcionalidades do carro

--- Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/script.js
+++ Fundamentos/Bloco_4-Introdução_a_Lógica_De_Programação_e_JavaScript/Dia_4/script.js
@@ -1,46 +1,114 @@
 //Exercícios Objetos
 
-let player = { 
-    name: "Marta",
-    lastName: "Silva",
-    age: 34,
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-    medals: {
-        golden: 2,
-        silver: 3
-    }
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
 };
 
-console.log("A jogadora " + player.name + " " + player.lastName + " " + "tem " + player.age + " " + "anos de idade.");
+console.log(
+  "A jogadora " +
+    player.name +
+    " " +
+    player.lastName +
+    " " +
+    "tem " +
+    player.age +
+    " " +
+    "anos de idade."
+);
 
-console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
+console.log(
+  "A jogadora " +
+    player.name +
+    " " +
+    player.lastName +
+    " foi eleita a melhor do mundo por " +
+    player.bestInTheWorld.length +
+    " vezes."
+);
 
-console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro" + " " + "e " + player.medals.silver + " medalhas de prata.");
+console.log(
+  "A jogadora possui " +
+    player.medals.golden +
+    " medalhas de ouro" +
+    " " +
+    "e " +
+    player.medals.silver +
+    " medalhas de prata."
+);
 
 // Exercícios ForIn
 
-let pizzas = {
-    sabor:"Palmito",
-    preco: 39.90,
-    bordaCatupiry: true
+let names = {
+  person1: "João",
+  person2: "Maria",
+  person3: "Jorge",
 };
 
-for(let key in pizzas) {
-    console.log(pizzas[key]);
+for (let index in names) {
+  console.log("Olá " + names[index]);
 }
 
-let pizzasDoces = ['chocolate', 'banana', 'morango'];
+let car = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
 
-for (let key in pizzasDoces) {
-    console.log(key, pizzasDoces[key]);
+for (let key in car) {
+  console.log(key, car[key]);
 }
 
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge' 
-  };
+//Exercícios Funções
 
-  for(let index in names) {
-      console.log("Olá " + names[index]);
+//Função Ligar e desligar
+var statusCarro = "desligado";
+var aceleracao = 0;
+var rotacaoVolante = 0;
+var statusFarol = "desligado";
+
+function ligarDesligar() {
+  if (statusCarro === "desligado") {
+    statusCarro = "ligado";
+  } else {
+    statusCarro = "desligado";
   }
+  return statusCarro;
+}
+
+//Função acelerar
+function acelerar(incremento) {
+  aceleracao = aceleracao + incremento;
+
+  return "Acelerando a " + aceleracao + " m/s².";
+}
+
+//Função frear
+function frear(decremento) {
+  aceleracao = aceleracao - decremento;
+
+  return "Desacelerando para " + aceleracao + " m/s².";
+}
+
+//Função girar volante
+function girarVolante(anguloRotacao) {
+  rotacaoVolante = anguloRotacao;
+  return rotacaoVolante + "graus";
+}
+
+//Função ligar e desligar faróis
+function ligarFarois() {
+    if(statusFarol === "desligado") {
+        statusFarol = "ligado";
+    } else {
+        statusFarol = "desligado"
+    }
+    return statusFarol;
+}
+

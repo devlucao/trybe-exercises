cb80b47a9077f07c24db47af068ee278a42f788d
Lucas Freitas <lucasr.freitas@icloud.com>
2021-07-11 15:18:21 -0300

    resolve exercicio de fixaçao

---
+++ Fundamentos/Bloco_5-JavaScript_DOM_Eventos-e-Web_Storage/Dia_1-Javascript-Dom_e_Seletores/fixacao.js
@@ -0,0 +1,8 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+document.querySelector("#footer-container").style.backgroundColor = "#2F4F4F";
+document.querySelector(".emergency-tasks").style.backgroundColor = "#FF00FF";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "#DBDB70";
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";

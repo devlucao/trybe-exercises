52d5be1f940a32a4fb14fbea25276c7aa7b714ab
Lucas Freitas <lucasr.freitas@icloud.com>
2021-08-31 13:00:04 -0300

    Resolve exercicio de fixaçao da aula

---
+++ Front-end/Bloco_11-Introdução_à_React/Dia_1-Introdução_Front-end/hello-world/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Nome extends Component {
+    render() {
+        return <> 
+        <h1>Lucas Freitas</h1>
+        <p>Aspirante a Dev FullStack.</p>
+        </>
+    }
+}
+
+export default Nome

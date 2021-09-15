069ec15c7bc4e753db3388542eec1298093ac489
Lucas Freitas <lucasr.freitas@icloud.com>
2021-09-15 16:11:50 -0300

    Resolve fancy button

--- Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
+++ Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function december() {
+  console.log('Em Dezembro de 81 botou os ingleses na roda')
 }
+
+function placarMundial() {
+  console.log('3x0 no Liverpool ficou marcado na história')
+}
+
+function maiorDoRio() {
+  console.log('E no Rio não tem outro igual, só o FLAMENGO é campeão mundial!')
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button onClick={december}>Parte 1</button>
+        <button onClick={placarMundial}>Parte 2</button>
+        <button onClick={maiorDoRio}>Parte 3</button>
+      </div>
+    );
+  }
+  }
 
 export default App;

41d149c033e2a2caf5e7debb9734388ffd576b79
Lucas Freitas <lucasr.freitas@icloud.com>
2021-09-15 16:55:28 -0300

    Cria funcao handleClick para exemplificar bind e atualizar botao com numero de cliques

--- Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
+++ Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
@@ -3,12 +3,21 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super()
-    this.december = this.december.bind(this);
-    console.log('Construindo componente')
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      numeroDeCliques: 0,
+    }
+  }
+
+  handleClick() {
+    console.log(this); // Para que a função enxergue o this é necessário realizar o bind dentro do constructor conforme linha 6. Sem o bind o console retorna undefined.
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+    console.log('Clicou')
   }
 
   december() {
-    console.log(this); // Para que a função enxergue o this é necessário realizar o bind dentro do constructor conforme linha 6.
     console.log('Em Dezembro de 81 botou os ingleses na roda')
   }
   
@@ -23,6 +32,7 @@ class App extends Component {
     return (
       <div className="App">
         {/* Quando queremos chamar uma função que se encontra dentro da classe precisamos utilizar o .this */}
+        <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
         <button onClick={this.december}>Parte 1</button>
         <button onClick={this.placarMundial}>Parte 2</button>
         <button onClick={this.maiorDoRio}>Parte 3</button>

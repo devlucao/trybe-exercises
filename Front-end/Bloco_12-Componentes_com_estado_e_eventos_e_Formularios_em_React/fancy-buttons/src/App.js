5b4e2198340927b6865c99a14c4ab3fe7f266ef4
Lucas Freitas <lucasr.freitas@icloud.com>
2021-09-15 16:17:07 -0300

    Utiliza this para chamar funçoes dentro da classe

--- Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
+++ Front-end/Bloco_12-Componentes_com_estado_e_eventos_e_Formularios_em_React/fancy-buttons/src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 
-function december() {
-  console.log('Em Dezembro de 81 botou os ingleses na roda')
-}
-
-function placarMundial() {
-  console.log('3x0 no Liverpool ficou marcado na história')
-}
-
-function maiorDoRio() {
-  console.log('E no Rio não tem outro igual, só o FLAMENGO é campeão mundial!')
-}
-
 class App extends Component {
+
+  december() {
+    console.log('Em Dezembro de 81 botou os ingleses na roda')
+  }
+  
+  placarMundial() {
+    console.log('3x0 no Liverpool ficou marcado na história')
+  }
+  
+  maiorDoRio() {
+    console.log('E no Rio não tem outro igual, só o FLAMENGO é campeão mundial!')
+  }
   render() {
+    console.log(this);
     return (
       <div className="App">
-        <button onClick={december}>Parte 1</button>
-        <button onClick={placarMundial}>Parte 2</button>
-        <button onClick={maiorDoRio}>Parte 3</button>
+        {/* Quando queremos chamar uma função que se encontra dentro da classe precisamos utilizar o .this */}
+        <button onClick={this.december}>Parte 1</button>
+        <button onClick={this.placarMundial}>Parte 2</button>
+        <button onClick={this.maiorDoRio}>Parte 3</button>
       </div>
     );
   }

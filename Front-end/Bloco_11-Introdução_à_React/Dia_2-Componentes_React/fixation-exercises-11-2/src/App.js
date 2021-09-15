3a04f145e7de59a32cf58e9c8b2c71c90976e597
Lucas Freitas <lucasr.freitas@icloud.com>
2021-09-15 11:14:34 -0300

    Finaliza exercicios de fixaçao do Dia 2

--- Front-end/Bloco_11-Introdução_à_React/Dia_2-Componentes_React/fixation-exercises-11-2/src/App.js
+++ Front-end/Bloco_11-Introdução_à_React/Dia_2-Componentes_React/fixation-exercises-11-2/src/App.js
@@ -2,35 +2,52 @@
 import React from 'react';
 import './App.css';
 import Order from './Order';
+import Image from './Image';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
+    const products = [
 
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+      {
+        name: 'headphone',
+        id: 102,
+        user: "cena@gmail.com",
+        product: "Razer Headphone",
+        price: {
+          value: 99.99,
+          currency: "dollars"
+        }
+      },
+  
+      {
+        name: 'Energy Drink',
+        id: 77,
+        user: "cena@gmail.com",
+        product: "Monster 500mL",
+        price: {
+          value: 9.99,
+          currency: "dollars"
+        }
+    },
+  ]
 
     return (
-      <div className="App">
-        <h1> Orders recently created </h1>
-         <Order order={headphone}/>
-         <Order order={energyDrink}/>
-      </div>
+      <section>
+        <div className="App">
+          <h1> Exercício 1 </h1>
+          <h2> Image </h2>
+          <Image 
+            source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" 
+            alternativeText="cute-cat"
+          />
+        </div>
+        <div>
+          <h1>Exercícios 2 e 3</h1>
+          <p> Exercício 2 - Renderizar orders; Exercício 3 - Criar componentes dinamicamente</p>
+          <h2>Orders recently created</h2>      
+            { products.map((prod) => <Order order={ prod } />)}        
+        </div>
+      </section>
     );
   }
 }
